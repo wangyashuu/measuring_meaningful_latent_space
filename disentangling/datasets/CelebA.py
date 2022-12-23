053621b8fcf297f19c1a397cdf18bf20da66451c
Yashu Wang <wang52@ads.uni-passau.de>
2022-12-23 17:40:30 +0000

    fix: fix test and run vae

--- disentangling/datasets/CelebA.py
+++ disentangling/datasets/CelebA.py
@@ -3,6 +3,8 @@ import torchvision.transforms as transforms
 
 
 def CelebA(root="data", input_size=64):
+    # gdown https://drive.google.com/uc?id=1m8-EBPgi5MRubrm6iQjafK2QMHDBMSfJ
+    # https://drive.google.com/file/d/1m8-EBPgi5MRubrm6iQjafK2QMHDBMSfJ/view?usp=sharing
     train_set = datasets.CelebA(
         root=root,
         transform=transforms.Compose(
