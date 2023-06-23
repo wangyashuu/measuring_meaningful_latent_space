ef811aefecc6c407cab859ee24e402efcd0c36e9
Yashu Wang <wang52@ads.uni-passau.de>
2023-06-23 12:15:12 +0000

    refactor: unsamplable datasets

--- disentangling/datasets/__init__.py
+++ disentangling/datasets/__init__.py
@@ -2,3 +2,6 @@ from .samplable_datasets.cars3d import cars3d
 from .samplable_datasets.chairs3d import chairs3d
 from .samplable_datasets.dSprites import dSprites
 from .samplable_datasets.shapes3d import shapes3d
+from .unsamplable_datasets.CelebA import CelebA
+from .unsamplable_datasets.MNIST import MNIST
+from .Generateds import generate
---
+++ disentangling/datasets/unsamplable_datasets/CelebA.py
@@ -0,0 +1,21 @@
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+from ...utils.data import train_test_split
+
+
+def CelebA(data_path="./data", input_size=64, **kwargs):
+    # gdown https://drive.google.com/uc?id=1m8-EBPgi5MRubrm6iQjafK2QMHDBMSfJ
+    # https://drive.google.com/file/d/1m8-EBPgi5MRubrm6iQjafK2QMHDBMSfJ/view?usp=sharing
+    transform = transforms.Compose(
+        [
+            transforms.CenterCrop(148),
+            transforms.Resize(input_size),
+            transforms.ToTensor(),
+        ]
+    )
+
+    dataset = datasets.CelebA(
+        root=data_path, transform=transform, download=True, split="all"
+    )
+    return train_test_split(dataset, **kwargs)
